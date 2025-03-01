9cafd0412f1fc5e98627024c7f5112a80f47a866
DevYemi <silasadegoke331@gmail.com>
2025-03-01 16:21:24 +0100

    feat: manifes file

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,23 +1,23 @@
 import type { Metadata } from "next";
-import { Montserrat,Roboto } from "next/font/google";
+import { Montserrat, Roboto } from "next/font/google";
 import "./globals.css";
 const montserrat = Montserrat({
   variable: "--montserrat",
   subsets: ["latin"],
-  weight: ["400", "700", "300","100","500","700","900"], 
-
+  weight: ["400", "700", "300", "100", "500", "700", "900"],
 });
 const roboto = Roboto({
   variable: "--roboto",
   subsets: ["latin"],
-  weight: ["400", "700", "300","100","500","700","900"],
-
+  weight: ["400", "700", "300", "100", "500", "700", "900"],
 });
 
 export const metadata: Metadata = {
   title: "Nano lounge",
   description:
     "Indulge in refined apartments or unwind with our exclusive pool services – tailored to your needs",
+  category: "Business",
+  manifest: "/manifest.json",
 };
 export const viewport = {
   themeColor: "#ffffff", // Use a valid hex color (e.g., white)
@@ -32,7 +32,9 @@ export default function RootLayout({
       <head>
         <meta name="color-scheme" content="light" />
       </head>
-      <body className={`${montserrat.variable} ${roboto.variable} antialiased`}>{children}</body>
+      <body className={`${montserrat.variable} ${roboto.variable} antialiased`}>
+        {children}
+      </body>
     </html>
   );
 }
